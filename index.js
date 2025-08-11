da6698df197ce67e6ae3145a381c86677ca77675
Rohit S <rohitishandsome07@gmail.com>
2025-08-11 12:16:51 +0530

    cors change

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ mongoose
 // ✅ Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
 
 // app.use(cors());
 app.get("/", (req, res) => {
